07ca2082604f3b04561d864869ed56b7658846a2
sousaryan1 <ryan.costa122@gmail.com>
2021-03-30 22:31:13 -0300

    Aula 2 - Preparando build para producao

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins = [];
+
+if(process.env.NODE_ENV == 'production') {
+    plugins.push(new babiliPlugin());
+}
 module.exports = {
     entry: './app-src/app.js',
     output: {
@@ -6,14 +13,16 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    }
+            rules: [
+                {
+                    test:/\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+
+                },
+            ]
+        },
+        plugins
 }
